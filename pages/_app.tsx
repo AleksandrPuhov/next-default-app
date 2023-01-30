d3a7d1afd569eececd379dbca10e5e3cf488184a
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-01-30 17:51:40 +0300

    add react querry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '@/app/styles/globalStyles';
 import { theme } from '@/app/styles/thems';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+            // staleTime: 0,
+          },
+        },
+      })
+  );
+
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,66 @@
-import { Cards } from '@/widgets/Cards';
+import { NextPage } from 'next';
 
-export default function HomePage() {
+import {
+  dehydrate,
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from 'react-query';
+
+import { Cards } from '@/widgets/Cards';
+import { fetchPosts, usePosts } from 'src/pages/hooks/usePosts';
+
+const HomePage: NextPage = () => {
+  const { data, isLoading, isFetching } = usePosts();
+
+  // console.log('isLoading', isLoading);
+  // console.log('isFetching', isFetching);
+
+  // console.log('isFetching', isFetching);
+
+  // const queryClient = useQueryClient();
+
+  // const addComment = useMutation(
+  //   (newTodo) => axios.post('/api/data', { text: newTodo }),
+  //   {
+  //     onMutate: () => {
+  //       console.log('sdfsfsdf');
+  //     },
+  //     onSuccess: () => {
+  //       console.log('update');
+  //       queryClient.invalidateQueries({ queryKey: 'posts' });
+  //     },
+  //   }
+  // );
+
   return (
     <>
       <p>HomePage</p>
+      <p>{data[0].title}</p>
+      <button>asdadadad</button>
       <Cards link={'/about'} name={'About Page'} />
     </>
   );
+};
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  // const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: {
+  //       staleTime: Infinity,
+  //     },
+  //   },
+  // });
+
+  await queryClient.prefetchQuery('posts', fetchPosts, { staleTime: 0 });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 }
+
+export default HomePage;
